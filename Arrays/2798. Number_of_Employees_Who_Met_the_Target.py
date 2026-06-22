c5785267ec758ada9421420d3a04e1e7f715c357
Rakshanaa Babu <rakshanaa247@gmail.com>
2026-06-22 20:21:57 +0530

    Add Number of Employees Who Met the Target

---
+++ Arrays/2798. Number_of_Employees_Who_Met_the_Target.py
@@ -0,0 +1,23 @@
+# Intuition
+<!-- Describe your first thoughts on how to solve this problem. -->
+
+# Approach
+<!-- Describe your approach to solving the problem. -->
+
+# Complexity
+- Time complexity:
+<!-- Add your time complexity here, e.g. $$O(n)$$ -->
+
+- Space complexity:
+<!-- Add your space complexity here, e.g. $$O(n)$$ -->
+
+# Code
+```python3 []
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        count=0
+        for i in hours:
+            if i>=target:
+                count+=1
+        return count
+```
